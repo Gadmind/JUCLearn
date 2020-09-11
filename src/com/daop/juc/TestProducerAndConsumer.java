fd480d935d534df09abdd03798fc7393a8fb9bdd
Gadmind <daopudu@sina.com>
2020-09-11 16:51:04 +0800

    测试生产者、消费者案例

--- src/com/daop/juc/TestProducerAndConsumer.java
+++ src/com/daop/juc/TestProducerAndConsumer.java
@@ -6,12 +6,20 @@ package com.daop.juc;
  * @Description: 测试生产者、消费者案例
  * @DATE: 2020-09-10
  * @AUTHOR: Administrator
+ * 等待唤醒机制 线程.wait()  线程.notifyAll()
  **/
 public class TestProducerAndConsumer {
+
     public static void main(String[] args) {
         Clerk clerk = new Clerk();
-        Producer producer = new Producer(clerk);
-        Consumer consumer = new Consumer(clerk);
+        Producer pro = new Producer(clerk);
+        Consumer cus = new Consumer(clerk);
+        //虚假唤醒
+        new Thread(pro, "生产者").start();
+        new Thread(cus, "消费者").start();
+
+        new Thread(pro, "生产者2").start();
+        new Thread(cus, "消费者2").start();
     }
 }
 
@@ -24,23 +32,27 @@ class Clerk {
     /**
      * 进货
      */
-    public synchronized void get() {
-        if (product >= 10) {
+    public synchronized void get() throws InterruptedException {
+        //避免虚假唤醒问题，wait()方法应该总是在循环中使用
+        while (product >= 1) {
             System.out.println("产品已满");
-        } else {
-            System.out.println(Thread.currentThread().getName() + ":" + ++product);
+            this.wait();
         }
+        System.out.println(Thread.currentThread().getName() + ":" + ++product);
+        this.notifyAll();
     }
 
     /**
      * 卖货
      */
-    public synchronized void sale() {
-        if (product <= 0) {
+    public synchronized void sale() throws InterruptedException {
+        while (product <= 0) {
+
             System.out.println("缺货");
-        } else {
-            System.out.println(Thread.currentThread().getName() + ":" + --product);
+            this.wait();
         }
+        System.out.println(Thread.currentThread().getName() + ":" + --product);
+        this.notifyAll();
     }
 }
 
@@ -54,7 +66,12 @@ class Producer implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 20; i++) {
-            clerk.get();
+            try {
+                Thread.sleep(200);
+                clerk.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -69,7 +86,11 @@ class Consumer implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 20; i++) {
-            clerk.sale();
+            try {
+                clerk.sale();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
