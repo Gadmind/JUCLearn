bf1080aa063580715087cf293fc1d1a2ba88ae04
Gadmind <daopudu@sian.com>
2020-09-12 22:11:56 +0800

    线程池

--- src/com/daop/juc/TestThreadPool.java
+++ src/com/daop/juc/TestThreadPool.java
@@ -1,22 +1,76 @@
 package com.daop.juc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 /**
  * @BelongsProject: JUCLearn
  * @BelongsPackage: com.daop.juc
  * @Description: 线程池
  * @DATE: 2020-09-11
  * @AUTHOR: Administrator
+ * <p>
  * 线程池：提供了一个线程队列，队列中保存着所有等待状态的线程。避免了创建与销毁的额外开销，从而提高了响应速度。
  * 体系结构：
  * java.util.concurrent.Executor：负责线程的使用与调度的根接口
- *      |--ExecutorService 子接口：线程池的主要接口
- *          |--AbstractExecutorService：抽象类同提供实现的默认方法
- *              |--ThreadPoolExecutor：实现类
- *          |--ScheduledThreadPoolExecutor 子接口：负责线程的调度
- *              |--ScheduledThreadPoolExecutor：继承了ThreadPoolExecutor，实现了ScheduleExecutorService
+ * |--ExecutorService 子接口：线程池的主要接口
+ * |--AbstractExecutorService：抽象类同提供实现的默认方法
+ * |--ThreadPoolExecutor：实现类
+ * |--ScheduledThreadPoolExecutor 子接口：负责线程的调度
+ * |--ScheduledThreadPoolExecutor：继承了ThreadPoolExecutor，实现了ScheduleExecutorService
+ * 工具类：Executors
+ * ExecutorService newFixedThreadPool()：创建固定线程池大小
+ * ExecutorService newCachedThreadPool()：缓存线程池，线程池的数量不固定，可以根据需求自动的更改数量
+ * ExecutorService newSingleThreadExecutor()：创建单个线程池。线程池中只有一个线程
+ * <p>
+ * ScheduledExecutorService newScheduledThreadPool() 创建固定大小的线程池，可以延迟或定时的任务
+ * </p>
  **/
 public class TestThreadPool {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
+        //创建线程池
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+
+        //tong通过Callable方法创建线程任务
+        List<Future<Integer>> list = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+
+            Future<Integer> future = pool.submit(new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                    int sum = 0;
+                    for (int i = 0; i <= 100; i++) {
+                        sum += i;
+                    }
+                    return sum;
+                }
+            });
+            list.add(future);
+        }
+        for (Future<Integer> fu : list) {
+            System.out.println(fu.get());
+        }
+
+        /*ThreadPoolDemo tp = new ThreadPoolDemo();
+        //为线程池中的线程分配任务
+        for (int i = 0; i < 10; i++) {
+            pool.submit(tp);
+        }*/
+        //关闭线程池
+        pool.shutdown();
+
+    }
+}
+
+class ThreadPoolDemo implements Runnable {
+    private int i = 0;
+
+    @Override
+    public void run() {
+        while (i <= 100) {
+            System.out.println(Thread.currentThread().getName() + "：----" + i++);
+        }
     }
 }
